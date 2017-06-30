7e766c102a7066e96958f9992bc8175867d8f551
spsmainali <saurav_prakash@hotmail.com>
2017-06-30 14:33:25 +0530

    Commit7_1

---
+++ src/Assign_7_1.java
@@ -0,0 +1,21 @@
+
+public class Assign_7_1 {
+	
+	    public static void main(String[] args) {
+	        
+	        /* Method 1: using valueOf() method
+	         * of String class.
+	         */
+	        int ivar = 123;
+	        String str = String.valueOf(ivar);
+	        System.out.println("String is: "+str);
+	        
+	        /* Method 2: using toString() method 
+	         * of Integer class
+	         */
+	        int ivar2 = 4546;
+	        String str2 = Integer.toString(ivar2);
+	        System.out.println("String2 is: "+str2);
+	    }
+	}
+
